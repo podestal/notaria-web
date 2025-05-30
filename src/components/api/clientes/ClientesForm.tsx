82ca48cfbde3521f299d5f1b61bba1fdeae4a73e
podestal <luisrodriguezpodesta@gmail.com>
2025-05-30 10:46:51 -0400

    cliente form ui

--- src/components/api/clientes/ClientesForm.tsx
+++ src/components/api/clientes/ClientesForm.tsx
@@ -1,16 +1,9 @@
 import { useState } from "react"
+import SimpleInput from "../../ui/SimpleInput"
 
 const ClientesForm = () => {
 
-    // idcontratante = models.CharField(primary_key=True, max_length=10)
-    // idcliente = models.CharField(max_length=10)
-    // tipper = models.CharField(max_length=1)
-    // apepat = models.CharField(max_length=100)
-    // apemat = models.CharField(max_length=100)
-    // prinom = models.CharField(max_length=100)
-    // segnom = models.CharField(max_length=100)
-    // nombre = models.CharField(max_length=1000)
-    // direccion = models.CharField(max_length=3000)
+
 
     const [apepat, setApepat] = useState('')
     const [apemat, setApemat] = useState('')
@@ -21,14 +14,143 @@ const ClientesForm = () => {
 
   return (
     <form>
-        <div>
-            <label className="block text-sm font-medium text-slate-700 mb-2">Apellido Paterno</label>
-            <input 
+        <h2 className="text-2xl font-bold text-center mb-10">Nuevo Cliente</h2>
+        <div className="flex justify-center items-center gap-6 mb-4">
+            <SimpleInput 
+                label="Apellido Paterno"
                 value={apepat}
-                onChange={(e) => setApepat(e.target.value)}
-                placeholder="Apellido Paterno"
-                className="w-full bg-white text-slate-700 border border-slate-300 rounded-md py-2 px-3 focus:border-blue-700 focus:outline-none"
+                setValue={setApepat}
+                horizontal={true}
             />
+            <SimpleInput 
+                label="Apellido Materno"
+                value={apemat}
+                setValue={setApemat}
+                horizontal={true}
+            />
+        </div>
+        <div className="flex justify-center items-center gap-6 mb-6">
+            <SimpleInput 
+                label="Primer Nombre"
+                value={prinom}
+                setValue={setPrinom}
+                horizontal={true}
+            />
+            <SimpleInput 
+                label="Segundo Nombre"
+                value={segnom}
+                setValue={setSegnom}
+                horizontal={true}
+            />
+        </div>
+        <div className="flex flex-col justify-center items-center gap-6 mb-4">
+            <SimpleInput 
+                label="Dirección"
+                value={direccion}
+                setValue={setDireccion}
+                horizontal={true}
+            />
+            <SimpleInput 
+                label="Ubigeo"
+                value={nombre}
+                setValue={setNombre}
+                horizontal={true}
+            />
+        </div>
+        <div className="flex justify-center items-center gap-6 mb-6">
+            <SimpleInput 
+                label="Estado Civil"
+                value={prinom}
+                setValue={setPrinom}
+                horizontal={true}
+            />
+            <SimpleInput 
+                label="Sexo"
+                value={segnom}
+                setValue={setSegnom}
+                horizontal={true}
+            />
+        </div>
+        <div className="flex justify-center items-center gap-6 mb-6">
+            <SimpleInput 
+                label="Nacionalidad"
+                value={prinom}
+                setValue={setPrinom}
+                horizontal={true}
+            />
+            <SimpleInput 
+                label="Residente"
+                value={segnom}
+                setValue={setSegnom}
+                horizontal={true}
+            />
+        </div>
+        <div className="flex justify-center items-center gap-6 mb-6">
+            <SimpleInput 
+                label="Natural de"
+                value={prinom}
+                setValue={setPrinom}
+                horizontal={true}
+            />
+            <SimpleInput 
+                label="Fecha de Nacimiento"
+                value={segnom}
+                setValue={setSegnom}
+                horizontal={true}
+            />
+        </div>
+        <div className="flex justify-center items-center gap-6 mb-6">
+            <SimpleInput 
+                label="Prof/Ocupación"
+                value={prinom}
+                setValue={setPrinom}
+                horizontal={true}
+            />
+            <button className="bg-gray-50 px-2 py-1 transition duration-300 text-xs border-1 border-gray-300 cursor-pointer hover:bg-gray-300 rounded-md">Seleccionar</button>
+        </div>
+        <div className="flex justify-center items-center gap-6 mb-6">
+            <SimpleInput 
+                label="Cargo"
+                value={prinom}
+                setValue={setPrinom}
+                horizontal={true}
+            />
+            <button className="bg-gray-50 px-2 py-1 transition duration-300 text-xs border-1 border-gray-300 cursor-pointer hover:bg-gray-300 rounded-md">Seleccionar</button>
+        </div>
+        <div className="flex justify-center items-center gap-6 mb-4">
+            <SimpleInput 
+                label="Teléfono Celular"
+                value={direccion}
+                setValue={setDireccion}
+                horizontal={true}
+            />
+            <SimpleInput 
+                label="Teléfono Oficina"
+                value={nombre}
+                setValue={setNombre}
+                horizontal={true}
+            />
+        </div>
+        <div className="flex justify-center items-center gap-6 mb-4">
+            <SimpleInput 
+                label="Teléfono Fijo"
+                value={direccion}
+                setValue={setDireccion}
+                horizontal={true}
+            />
+            <SimpleInput 
+                label="Email"
+                value={nombre}
+                setValue={setNombre}
+                horizontal={true}
+            />
+        </div>
+        <div className="flex justify-center items-center gap-6 mb-4">
+            <button 
+                type="submit"
+                className="mt-8 bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors duration-300">
+                Guardar Cliente
+            </button>
         </div>
     </form>
   )
--- src/components/ui/SimpleInput.tsx
+++ src/components/ui/SimpleInput.tsx
@@ -1,17 +1,22 @@
+interface Props {
+    value: string
+    setValue: (value: string) => void
+    label?: string 
+    horizontal?: boolean
+}
 
+const SimpleInput = ({ value, setValue, label, horizontal }: Props) => {
+  return (
+    <div className={`flex w-full ${horizontal ? 'flex-row justify-baseline items-center' : 'flex-col justify-center items-start'} gap-6`}>
+        {label && <label className="pl-2 block text-xs font-semibold text-slate-700 mb-2">{label}</label>}
+        <input 
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder={label}
+            className="w-full bg-white text-slate-700 border border-slate-300 rounded-md py-2 px-3 focus:border-blue-700 focus:outline-none"
+        />
+    </div>
+  )
+}
 
-// const SimpleInput = () => {
-//   return (
-//     <div>
-//         <label className="block text-sm font-medium text-slate-700 mb-2">Apellido Paterno</label>
-//         <input 
-//             value={apepat}
-//             onChange={(e) => setApepat(e.target.value)}
-//             placeholder="Apellido Paterno"
-//             className="w-full bg-white text-slate-700 border border-slate-300 rounded-md py-2 px-3 focus:border-blue-700 focus:outline-none"
-//         />
-//     </div>
-//   )
-// }
-
-// export default SimpleInput
+export default SimpleInput
