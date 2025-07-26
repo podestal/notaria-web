d9e0fca3022fcc7ad6f9934d4a5bc305d25148c4
podestal <luisrodriguezpodesta@gmail.com>
2025-07-26 07:01:16 -0400

    legalizacion filters

---
+++ src/components/api/extraprotocolares/legalizacion/LegalizacionFilters.tsx
@@ -0,0 +1,28 @@
+import Calendar from "../../../ui/Calendar";
+
+interface Props {
+    dateFrom: Date | undefined;
+    setDateFrom: React.Dispatch<React.SetStateAction<Date | undefined>>
+    dateTo: Date | undefined;
+    setDateTo: React.Dispatch<React.SetStateAction<Date | undefined>>;
+}
+
+const LegalizacionFilters = ({ dateFrom, setDateFrom, dateTo, setDateTo }: Props ) => {
+  return (
+    <div className="w-full grid grid-cols-3 gap-4 justify-center items-center text-center my-6">
+        <div className="flex flex-col justify-center items-center gap-4 font-semibold text-sm">
+            <p>Fecha Inicio</p>
+            <Calendar selectedDate={dateFrom} setSelectedDate={setDateFrom} />
+        </div>
+        <div className="flex flex-col justify-center items-center gap-4 font-semibold text-sm">
+            <p>Fecha Fin</p>
+            <Calendar selectedDate={dateTo} setSelectedDate={setDateTo} />
+        </div>
+        <div>
+            <button className="bg-blue-500 text-white rounded-lg py-2 px-4 hover:bg-blue-600 cursor-pointer">Buscar</button>
+        </div>
+    </div>
+  )
+}
+
+export default LegalizacionFilters
--- src/components/api/extraprotocolares/legalizacion/LegalizacionMain.tsx
+++ src/components/api/extraprotocolares/legalizacion/LegalizacionMain.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import GenericHeader from "../../../ui/GenericHeader"
+import LegalizacionFilters from "./LegalizacionFilters"
 import LegalizacionTable from "./LegalizacionTable"
 
 
 const LegalizacionMain = () => {
+
+  const [dateFrom, setDateFrom] = useState<Date | undefined>(undefined)
+  const [dateTo, setDateTo] = useState<Date | undefined>(undefined)
+
   return (
     <div className="mt-[80px] w-[85%] mx-auto bg-slate-100 rounded-lg shadow-lg mb-10 text-black">
         <GenericHeader 
           title="Legalización de Firmas"
           setOpen={() => {}}
         />
-        <p>Filters</p>
+        <LegalizacionFilters 
+          dateFrom={dateFrom}
+          setDateFrom={setDateFrom}
+          dateTo={dateTo}
+          setDateTo={setDateTo}
+        />
         <LegalizacionTable />
     </div>
   )
