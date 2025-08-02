ce1ecf290d74b30315620fa99ecc5590e9772807
podestal <luisrodriguezpodesta@gmail.com>
2025-08-02 08:08:07 -0400

    passing carta data to form when update

--- src/components/api/extraprotocolares/cartasNotariales/CartasNotarialesCard.tsx
+++ src/components/api/extraprotocolares/cartasNotariales/CartasNotarialesCard.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { IngresoCartas } from "../../../../services/api/extraprotocolares/ingresoCartas"
+import TopModal from "../../../ui/TopModal";
+import PreCartasNotarialesForm from "./PreCartasNotarialesForm";
 
 interface Props {
     carta: IngresoCartas
@@ -6,14 +9,28 @@ interface Props {
 }
 
 const CartasNotarialesCard = ({ carta }: Props) => {
+
+  const [open, setOpen] = useState(false);
+
   return (
-        <div className="grid grid-cols-7 gap-4 justify-center items-center text-center text-black text-xs p-2 my-4 mx-6">
-        <div>{carta.num_carta}</div>
-        <div>{carta.fec_ingreso}</div>
-        <div>{carta.fec_entrega}</div>
-        <div className="col-span-2">{carta.nom_remitente}</div>
-        <div className="col-span-2">{carta.nom_destinatario}</div>
-    </div>
+      <>
+      <div className="grid grid-cols-7 gap-4 justify-center items-center text-center text-black text-xs p-2 my-4 mx-6">
+        <p
+          onClick={() => setOpen(true)}
+          className="text-center text-blue-600 cursor-pointer hover:text-blue-500"
+        >{carta.num_carta}</p>
+        <p>{carta.fec_ingreso}</p>
+        <p>{carta.fec_entrega}</p>
+        <p className="col-span-2">{carta.nom_remitente}</p>
+        <p className="col-span-2">{carta.nom_destinatario}</p>
+      </div>
+      <TopModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+      >
+        <PreCartasNotarialesForm carta={carta} />
+      </TopModal>
+      </>
   )
 }
 
