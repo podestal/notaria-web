ddbfcc3e0a4292df5a4bae372bfaf3f1c1e8260d
podestal <luisrodriguezpodesta@gmail.com>
2025-09-05 09:20:51 -0400

    fixing garantias for NC

--- src/components/api/sisgen/SisgenMain.tsx
+++ src/components/api/sisgen/SisgenMain.tsx
@@ -10,8 +10,8 @@ const SisgenMain = () => {
                 // { id: 'general', label: 'Kardex Info', content: <KardexForm createKardex={createKardex} setNotAllowed={setNotAllowed} /> },
                 { id: 'escrituras', label: 'Escrituras', content: <SisgenBody typekardex="Escrituras Públicas" instrumentType={1} />},
                 { id: 'transferencias', label: 'Transferencias', content: <SisgenBody typekardex="Transferencias Vehiculares" instrumentType={3} /> },
-                { id: 'garantias', label: 'Garantías', content: <SisgenBody typekardex="Garantías Inmobiliarias" instrumentType={2} /> },
-                { id: 'no-contenciosos', label: 'No Contenciosos', content: <SisgenBody typekardex="No Contenciosos" instrumentType={4} /> },
+                { id: 'garantias', label: 'Garantías', content: <SisgenBody typekardex="Garantías Inmobiliarias" instrumentType={4} /> },
+                { id: 'no-contenciosos', label: 'No Contenciosos', content: <SisgenBody typekardex="No Contenciosos" instrumentType={2} /> },
                 { id: 'libros', label: 'Libros', content: <SisgenBody typekardex="Libros Contables" instrumentType={5} /> },
             ]}
         />
