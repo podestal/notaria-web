790aa5ef1d9ad109c9dac2c05d03232a2dc40c33
podestal <luisrodriguezpodesta@gmail.com>
2025-09-18 09:06:39 -0400

    acto added to table

--- src/components/api/reportes/archivosPdt/pdtKardex/ArchivosPdtKardexBody.tsx
+++ src/components/api/reportes/archivosPdt/pdtKardex/ArchivosPdtKardexBody.tsx
@@ -9,9 +9,10 @@ const ArchivosPdtKardexBody = ({ errors }: Props) => {
     console.log('errors', errors)
   return (
     <div className="w-[85%] mx-auto pb-10 text-black">
-        <div className="grid grid-cols-2 gap-4 justify-center items-center text-center text-xs font-semibold p-2 my-4 mx-6 bg-slate-200 text-black">
+        <div className="grid grid-cols-5 gap-4 justify-center items-center text-center text-xs font-semibold p-2 my-4 mx-6 bg-slate-200 text-black">
             <p>Kardex</p>
-            <p>Descripción del Error</p>
+            <p className="col-span-2">Acto</p>
+            <p className="col-span-2">Descripción del Error</p>
         </div>
         {errors.map((error, index) => (
             <ArchivosPdtKardexCard key={index + error.kardex} error={error} />
--- src/components/api/reportes/archivosPdt/pdtKardex/ArchivosPdtKardexCard.tsx
+++ src/components/api/reportes/archivosPdt/pdtKardex/ArchivosPdtKardexCard.tsx
@@ -13,14 +13,15 @@ const ArchivosPdtKardexCard = ({ error }: Props) => {
     const [open, setOpen] = useState(false)
   return (
     <>
-    <div className="grid grid-cols-2 gap-4 justify-center items-center text-center text-xs p-2 my-4 mx-6 text-black">
+    <div className="grid grid-cols-5 gap-4 justify-center items-center text-center text-xs p-2 my-4 mx-6 text-black">
         <p 
             onClick={() => setOpen(true)}
             className="text-blue-500 hover:text-blue-700 cursor-pointer transition-all duration-300"
         >
             {error.kardex}
         </p>
-        <p>{error.errorItem}</p>
+        <p className="col-span-2">{error.typeAct}</p>
+        <p className="col-span-2">{error.errorItem}</p>
     </div>
     <TopModal
         isOpen={open}
