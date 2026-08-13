2e2aab62a722223c0bd61d19e668e7430a8c353b
podestal <luisrodriguezpodesta@gmail.com>
2026-08-13 09:57:31 -0400

    fixing filter estado sisgen

--- app/src/components/ui/SimpleSelector.tsx
+++ app/src/components/ui/SimpleSelector.tsx
@@ -46,9 +46,10 @@ const SimpleSelector = ({
 
   return (
     <div className="flex flex-col gap-2">
-        <div className="grid grid-cols-3 items-center gap-2 w-full">
-            <p className="pl-2 block text-xs font-semibold text-slate-700">{label}</p>
-            <div className="flex items-center gap-1 w-full col-span-2">
+        {label ? (
+        <div className="grid w-full grid-cols-3 items-center gap-2">
+            <p className="block pl-2 text-xs font-semibold text-slate-700">{label}</p>
+            <div className="col-span-2 flex w-full items-center gap-1">
                 <motion.select 
                     {...(error ? shakeAnimation : {})}
                     defaultValue={defaultValue ? defaultValue.toString() : '0'}
@@ -58,8 +59,7 @@ const SimpleSelector = ({
                         setError?.('');
                         setter(parseInt(e.target.value));
                     }}
-                    // className="flex-1 bg-white w-2xl col-span-2 text-slate-700 border border-slate-300 rounded-md py-2  px-2"
-                    className={`flex-1 bg-white text-slate-700 border rounded-md py-2 px-2 w-full ${
+                    className={`w-full flex-1 rounded-md border bg-white px-2 py-2 text-slate-700 ${
                         error ? 'border-red-500 focus:ring-red-300' : 'border-slate-300 focus:ring-blue-300'
                     } focus:outline-none focus:ring-2 ${disabled ? 'cursor-not-allowed bg-slate-100 text-slate-500' : ''}`}
                 >
@@ -72,9 +72,33 @@ const SimpleSelector = ({
                 {required && <span className="text-red-500">*</span>}
             </div>
         </div>
+        ) : (
+        <div className="flex w-full items-center gap-1">
+            <motion.select
+                {...(error ? shakeAnimation : {})}
+                defaultValue={defaultValue ? defaultValue.toString() : '0'}
+                value={defaultValue !== undefined ? defaultValue?.toString() : '0'}
+                disabled={disabled}
+                onChange={(e) => {
+                    setError?.('');
+                    setter(parseInt(e.target.value));
+                }}
+                className={`w-full rounded-md border bg-white px-2 py-2 text-slate-700 ${
+                    error ? 'border-red-500 focus:ring-red-300' : 'border-slate-300 focus:ring-blue-300'
+                } focus:outline-none focus:ring-2 ${disabled ? 'cursor-not-allowed bg-slate-100 text-slate-500' : ''}`}
+            >
+                {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </motion.select>
+            {required && <span className="text-red-500">*</span>}
+        </div>
+        )}
         {error && (
             <motion.div
-                className="text-xs text-center text-red-500 mt-1 px-2"
+                className="mt-1 px-2 text-center text-xs text-red-500"
                 initial={{ opacity: 0, y: -5 }}
                 animate={{ opacity: 1, y: 0 }}
                 >
